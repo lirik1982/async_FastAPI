1f0860f5981a3e59152f2254638d045783505338
KirillDegtyarev <lirik19@yandex.ru>
2023-09-28 23:28:08 +0700

    first commit

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, BackgroundTasks
+
 import uvicorn
-import asyncio
+
 from tasks import Tasks
-from fastapi import BackgroundTasks
 # Написать апи в котором стартует выполнение вычисления арифметической прогрессии.
 # 1 метод стартует прогрессию, параметры n1 n d и временной интервал между вычислениями.
 # 2 метод вывод статуса всех текущих вычислений.
 # N1 N d это первый элемент прогрессии, кол-во итераций, дельта он же шаг прогрессии
 
-# Тут не нужно глобальную, ты при создании сервера в инстанс приложения можно всунуть свой класс с тасками.
-# app.yourclass=YourClacc()
-# И потом в реквесте доставать из app либо через depends если это fastapi
-# Либо можно сделать синглтоном
-
-
 app = FastAPI()
 app.tasks = Tasks()
 
 
-# @app.get("/gettasks")
-async def get_tasks_api(request: Request):
-    return request.app.tasks
-
-
-@app.get("/addtask")
+@app.get("/addtask/")
 async def addtask(
-    N: int = 10, N1: int = 1, step: int = 1, interval: int = 1,
-    task_api: Tasks = Depends(get_tasks_api),
+    N=10, N1=1, step=1, interval=1,
+    task_api: Tasks = Depends(),
 ):
-    task_api.add_task(N=N, N1=N1, step=step, interval=interval)
-    # back_ground_tasks.add_task(mytask, N, N1, step, interval)
+    print(N, N1, step, interval)
+    task_api.add_task(
+        N=N, N1=N1, step=step, interval=interval)
     return {
         "response": "started",
     }
+
+# @app.get("/gettasks")
+# async def get_tasks_api(request: Request):
+#     return request.app.tasks
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -1,14 +1,17 @@
 import asyncio
-from singleton import singleton
-from fastapi import BackgroundTasks
+from fastapi import BackgroundTasks, Request
 
 
-@singleton
 class Tasks:
 
     def __init__(self) -> None:
         self.counter = 0
         self.status = {}
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Tasks, cls).__new__(cls)
+        return cls.instance
 
     async def _mytask(self, iters, start_from, step, interval):
         print("Начали задачу")
@@ -18,15 +21,19 @@ class Tasks:
             print(f'task step={i}  progress={current}')
             await asyncio.sleep(interval)
 
-    async def add_task(self,
-                       back_ground_tasks: BackgroundTasks,
-                       N: int = 10, N1: int = 1, step: int = 1, interval: int = 1,
-                       ):
+    async def add_task(
+        self,
+        back_ground_tasks=BackgroundTasks,
+        N=10, N1=1, step=1, interval=1,
+    ):
         print("class starting")
-        await back_ground_tasks.add_task(self.mytask, N, N1, step, interval)
+        await back_ground_tasks.add_task(self._mytask, N, N1, step, interval)
         return {
             "response": "started",
         }
+
+    async def get_tasks_api(request: Request):
+        return request.app.tasks
 
     def __update_status(self, iters, start_from, step):
 
