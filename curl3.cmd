b258d2034759aeb968e38b8ff28444c1e8a53a9b
KirillDegtyarev <lirik19@yandex.ru>
2023-09-30 23:58:53 +0700

    last-one

---
+++ curl3.cmd
@@ -0,0 +1,2 @@
+curl -X POST "http://localhost:8080/api/v1/tasks/add/" -H "Content-Type: application/json" -d @data.json 
+pause
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ N1 N d это первый элемент прогрессии,
 кол-во итераций, дельта он же шаг прогрессии
 """
 
-import uvicorn
+# import uvicorn
 from fastapi import FastAPI
 
 from routers import router
@@ -23,11 +23,11 @@ app.tasks = Tasks()
 app.include_router(router, prefix='/api/v1/tasks')
 
 
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='127.0.0.1',
-        log_level='info',
-        port=8000,
-        workers=1,
-    )
+# if __name__ == '__main__':
+#     uvicorn.run(
+#         'main:app',
+#         host='127.0.0.1',
+#         log_level='info',
+#         port=80,
+#         workers=1,
+#     )
